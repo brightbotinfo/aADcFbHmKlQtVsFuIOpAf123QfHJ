ccd343502143d6d869c565a061c150530a5c42f2
brightbotinfo <41553852+brightbotinfo@users.noreply.github.com>
2018-07-24 19:12:07 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,7 +81,7 @@ client.on("message", message => {
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['!.invite','!.help','قم بالتجربة ولن تندم','By Abo Khalil','يعمل بواسطة أبو خليل'];
+    var setGame = ['!.invite','!.help','my prefix is "!."','By Abo Khalil','التحديث غدا ان شاء الله'];
     var i = -1;
     var j = 0;
     setInterval(function (){
